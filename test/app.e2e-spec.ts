5b05e98f592365b3ec232f34d9b2495113d02414
qiaoyuwen <qiao.yuwen@foundbyte.com>
2021-04-08 17:51:12 +0800

    feat: add app e2e test init

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,32 @@
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
 import type { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+class MockConfigService {
+  get(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234567.',
+      database: 'elm',
+      entities: ['./src/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: false,
+      extra: {
+        // If without this filed emoji can't be stored
+        charset: 'utf8mb4_general_ci',
+      },
+    };
+  }
+}
+
+const mockConfigService = new MockConfigService();
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -10,13 +34,20 @@ describe('AppController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(ConfigService)
+      .useValue(mockConfigService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+  it('/shop-categories (GET)', () => {
+    return request(app.getHttpServer()).get('/shop-categories').expect(401);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
