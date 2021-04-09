e0d786c110312bd1fcc5655e731bb81d22903cc2
qiaoyuwen <qiao.yuwen@foundbyte.com>
2021-04-09 20:24:28 +0800

    feat: add comment

--- src/shop-categories/dto/find-all-shop-categories.request.dto.ts
+++ src/shop-categories/dto/find-all-shop-categories.request.dto.ts
@@ -2,11 +2,17 @@ import { validateIfNotUndefined } from './../../utils/class-validator/index';
 import { IsInt, IsPositive, ValidateIf } from 'class-validator';
 
 export class FindAllShopCategoriesRequestDTO {
+  /**
+   * 类别分级
+   */
   @ValidateIf(validateIfNotUndefined('level'))
   @IsPositive()
   @IsInt()
   level?: number;
 
+  /**
+   * 父类别ID
+   */
   @ValidateIf(validateIfNotUndefined('parentId'))
   @IsPositive()
   @IsInt()
